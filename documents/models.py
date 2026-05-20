b5e4d43323b268bf54834cecb3aee57a59e863d7
DaniyalIranMehr <daniyaliranmehr@gmail.com>
2026-05-20 14:47:49 +0330

    Implement automatic text extraction for uploaded DOCX files

--- documents/models.py
+++ documents/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from .utils import extract_text_from_docx
+
 
 class Document(models.Model):
     title = models.CharField(max_length=255)
     file = models.FileField(upload_to='documents/')
     content = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+
+        super().save(*args, **kwargs)
+
+        if self.file.name.endswith('.docx'):
+            self.content = extract_text_from_docx(self.file.path)
+
+        super().save(update_fields=['content'])
 
     def __str__(self):
         return self.title
