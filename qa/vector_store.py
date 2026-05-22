49d39d798492f38770fe43f06dd1a6f95c284f5e
DaniyalIranMehr <daniyaliranmehr@gmail.com>
2026-05-22 16:48:39 +0330

    Add HF Token to the project

--- qa/vector_store.py
+++ qa/vector_store.py
@@ -2,6 +2,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 
+
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
