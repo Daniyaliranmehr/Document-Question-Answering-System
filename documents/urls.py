7da3edfa366af042c20a3a3707cc413b8a7febd2
DaniyalIranMehr <daniyaliranmehr@gmail.com>
2026-05-24 19:55:35 +0330

    REST API - Get one specific document

--- documents/urls.py
+++ documents/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_documents
+from .views import list_documents, get_document
 
 urlpatterns = [
     path('', list_documents),
+    path('<int:document_id>/', get_document)
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from .models import Document
+from django.shortcuts import get_object_or_404
+
 
 def list_documents(request):
 
@@ -16,3 +18,17 @@ def list_documents(request):
         })
 
     return JsonResponse(data, safe=False)
+
+
+def get_document(request, document_id):
+
+    doc = get_object_or_404(Document, id=document_id)
+
+    data = {
+        "id": doc.id,
+        "title": doc.title,
+        "content": doc.content,
+        "created_at": doc.created_at,
+    }
+
+    return JsonResponse(data)
